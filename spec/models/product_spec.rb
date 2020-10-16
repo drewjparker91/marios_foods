6d99a578f36ee65826090fd1fdba67af479122f9
drewparker <drewjparker91@gmail.com>
2020-10-16 10:52:58 -0700

    create test in product_spec.rb for titleizing name of a product, get good fail and good pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,8 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :origin }
+  it("titleizes the name and origin of a product") do
+    product = Product.create({name: "oatmeal", cost: 2, origin: "United States"})
+    expect(product.name()).to(eq("Oatmeal"))
   end
+end
