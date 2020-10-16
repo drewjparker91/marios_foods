cc767c8ab5da8edc18a0b9dbfc05196e528ab53e
drewparker <drewjparker91@gmail.com>
2020-10-16 15:45:12 -0700

    create a landing page in a landing folder in views

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,9 @@
+class LandingController < ApplicationController
+
+  def index
+    @products = Product.all
+    @most_reviewed = Product.most_reviews
+    @most_recently_added = Product.most_recent
+    render :index
+  end
+end
