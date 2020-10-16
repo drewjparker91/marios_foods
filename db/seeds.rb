57222c3f4c38ac422281a22fefd2fcfc16be9d9d
drewparker <drewjparker91@gmail.com>
2020-10-16 15:06:07 -0700

    complete seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.destroy_all
-
-50.times do |index|
-  Product.create!(name: Faker::Food.unique.ingredient, cost: Faker::Number.decimal(r_digits:1), origin: Faker::Address.country)
-end
-
 Review.destroy_all
 
-250.times do |index|
-  Review.create!(author: Faker::Name.unique.name, rating: Faker::Number.between(from: 1, to: 5).to_i, content: Faker::Lorem.paragraph_by_chars(number:55))
+50.times do |index|
+  product = Product.create!(name: Faker::Food.unique.ingredient, cost: Faker::Number.decimal(r_digits:1), origin: Faker::Address.country)  
+  5.times do
+    product.reviews.new(author: Faker::Name.unique.name, rating: Faker::Number.between(from: 1, to: 5).to_i, content: Faker::Lorem.paragraph_by_chars(number: 50), product_id: Faker::Number.between(from: 1, to: 50))
+    product.save
+  end
 end
+
+# 250.times do |index|
+#   Review.create!(author: Faker::Name.unique.name, rating: Faker::Number.between(from: 1, to: 5).to_i, content: Faker::Lorem.paragraph_by_chars(number: 50), product_id: Faker::Number.between(from: 1, to: 50))
+# end
+
+
+# Review.destroy_all
+
+# 250.times do |index|
+#   Review.create!(author: Faker::Name.unique.name, rating: Faker::Number.between(from: 1, to: 5).to_i, content: Faker::Lorem.paragraph_by_chars(number: 50), product_id: Faker::Number.between(from: 1, to: 50))
+# end
+
+p "Created #{Product.count} products and #{Review.count} reviews"
