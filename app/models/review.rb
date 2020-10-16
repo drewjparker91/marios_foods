6938d8baa82905c767b709358b54538bd3d55f36
drewparker <drewjparker91@gmail.com>
2020-10-16 11:40:32 -0700

    add validations and work on creating a number range between 1-5 for ratings

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :rating, presence: true
+  validates :content, presence: true
+  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  before_save(:titleize_review)
+
+  private
+  def titleize_review
+    self.author = self.author.titleize
+  end
 end
