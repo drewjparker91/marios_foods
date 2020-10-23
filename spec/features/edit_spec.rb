cf8fc2cb20e71bc0639759c74d80b44bb77c9670
drewparker <drewjparker91@gmail.com>
2020-10-23 15:51:07 -0700

    add a checbox into views/registrations/new in order to trip admin boolean to pass tests

--- spec/features/edit_spec.rb
+++ spec/features/edit_spec.rb
@@ -1,18 +1,26 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "editing reviews and products" do
+describe "editing reviews and products" do
 
-#   before :each do
-#     @product = Product.create({:name => "Dilly Bar", :origin => "Mexico", :cost => 1.99})
-#     @review = Review.create(author: "Billy Bob", rating: 5, content: "asdf;lkjasfdl;kjasdflkjasdf;lkjasdfl;kjasdf;lkjasdf", product_id: @product.id)
-#   end
+  before :each do
+    @product = Product.create({:name => "Dilly Bar", :origin => "Mexico", :cost => 1.99})
+    # @review = Review.create(author: "Billy Bob", rating: 5, content: "asdf;lkjasfdl;kjasdflkjasdf;lkjasdfl;kjasdf;lkjasdf", product_id: @product.id)
+  end
 
-#   it "edits a product" do
-#     visit edit_product_path(@product)
-#     fill_in "product[name]", :with => "Banana Pancake Mix"
-#     fill_in "product[cost]", :with => 2.99
-#     fill_in "product[origin]", :with => "Ecuador"
-#     click_on "Update Product"
-#     expect(page).to have_content "Banana Pancake Mix"
-#   end
-# end
+  it "edits a product" do
+    visit '/'
+    click_link 'Sign-in or Sign-up'
+    click_on 'Sign up'
+    fill_in 'user[email]', :with => 'abc123@gmail.com'
+    fill_in 'user[password]', :with => 'abc123'
+    fill_in 'user[password_confirmation]', :with => 'abc123'
+    check 'user[admin]'
+    click_on 'Sign up'
+    visit edit_product_path(@product)
+    fill_in "product[name]", :with => "Dingus"
+    fill_in "product[cost]", :with => 6.99
+    fill_in "product[origin]", :with => "Mexico"
+    click_on "Update Product"
+    expect(page).to have_content "Dingus"
+  end
+end
