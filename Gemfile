32fd7caa7817793819b3125bec419b4204348ba4
drewparker <drewjparker91@gmail.com>
2020-10-23 16:44:22 -0700

    begin to implement pagination on products index and products controller, not fully functional yet

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem 'sassc-rails', '>= 2.1.0'
 
 gem 'devise'
 
+gem 'kaminari'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   before_action :only => [:new, :destroy, :edit] do
     redirect_to new_user_session_path unless current_user && current_user.admin
-
   end
+  
   def index
-    @products = Product.all
+    @products = Product.order(:name).page(params[:page])
     @user = current_user
   end
 
