c1dde803c25214a727d2e945e1efbb1909a832d2
drewparker <drewjparker91@gmail.com>
2020-10-23 16:20:52 -0700

    create test for adding a review to a product and passing test

--- spec/features/new_spec.rb
+++ spec/features/new_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
-describe "adds a new product to the application" do
+describe "adds a new products to the application" do
+
+  before :each do
+    @product = Product.create({:name => "Dilly Bar", :origin => "Mexico", :cost => 1.99})
+  end  
   
   it "adds a new product" do
-    visit'/'
     visit '/'
     click_link 'Sign-in or Sign-up'
     click_on 'Sign up'
@@ -19,6 +22,25 @@ describe "adds a new product to the application" do
     fill_in 'product[origin]', :with => 'United States of America'
     click_on 'Create Product'
     expect(page).to have_content 'Broad Toad'
+  end
 
+  it "adds a new review to a product" do
+    visit '/'
+    click_link 'Sign-in or Sign-up'
+    click_on 'Sign up'
+    fill_in 'user[email]', :with => 'abc123@gmail.com'
+    fill_in 'user[password]', :with => 'abc123'
+    fill_in 'user[password_confirmation]', :with => 'abc123'
+    check 'user[admin]'
+    click_on 'Sign up'
+    visit product_path(@product)
+    click_link 'Add a Review'
+    fill_in 'review[author]', :with => 'Brandolf' 
+    fill_in 'review[rating]', :with => 3
+    fill_in 'review[content]', :with => "alsdk;fjalsdkjf al;ksdjf;laksjdf ;lkasdjfl;kjasdf;lkjasd;flkjasd;lfkjasd;lfkj"
+    click_on 'Create Review'
+    expect(page).to have_content "Brandolf"
   end
 end
+
+
